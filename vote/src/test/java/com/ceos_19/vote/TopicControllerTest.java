2413ecfd8f4e4caf12c5888c6bd1405a71b1cd34
parking0 <p980710@gmail.com>
2024-06-12 16:17:21 +0900

    test : TopicController 테스트

--- vote/src/test/java/com/ceos_19/vote/TopicControllerTest.java
+++ vote/src/test/java/com/ceos_19/vote/TopicControllerTest.java
@@ -1,11 +1,5 @@
 package com.ceos_19.vote;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import com.ceos_19.vote.domain.VotingOption;
 import com.ceos_19.vote.dto.TopicResponse;
 import com.ceos_19.vote.service.TopicService;
 import org.junit.jupiter.api.Test;
@@ -13,10 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,23 +28,49 @@ public class TopicControllerTest {
     @MockBean
     private TopicService topicService;
 
+    /**
+     * 모든 Topic 반환하는지 확인
+     */
     @Test
-    public void getAllTopics_ReturnsOkAndTopics() throws Exception {
-        VotingOption votingOption1 = new VotingOption(1L, "리사");
+    @WithMockUser(username = "user", roles = "USER")
+    public void getAllTopics() throws Exception {        //topic, votingoption 양방향 참조 주의
 
-        TopicResponse topic1 = new TopicResponse(1L, "파트장 투표", 10, );
-        TopicResponse topic2 = new TopicResponse(2L, "데모데이 투표", 20);
-        List<TopicResponse> topics = Arrays.asList(topic1, topic2);
+        List<TopicResponse> topicResponses = new ArrayList<>();
+        topicResponses.add(new TopicResponse(1L, "파트장 투표", 10, new ArrayList<>()));
+        topicResponses.add(new TopicResponse(2L, "데모데이 투표", 20, new ArrayList<>()));
 
-        when(topicService.getAllTopics()).thenReturn(topics);
+        given(topicService.getAllTopics()).willReturn(topicResponses);
 
-        mockMvc.perform(get("/topics"))
+        // when/then
+        mockMvc.perform(get("/api/topics"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(topic1.getId()))
-                .andExpect(jsonPath("$[0].name").value(topic1.getName()))
-                .andExpect(jsonPath("$[0].description").value(topic1.getDescription()))
-                .andExpect(jsonPath("$[1].id").value(topic2.getId()))
-                .andExpect(jsonPath("$[1].name").value(topic2.getName()))
-                .andExpect(jsonPath("$[1].description").value(topic2.getDescription()));
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].name").value("파트장 투표"))
+                .andExpect(jsonPath("$[0].minimumVotesRequired").value(10))
+                .andExpect(jsonPath("$[0].votingOptionDto").isArray())  // votingOptionDto가 배열인지 확인
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].name").value("데모데이 투표"))
+                .andExpect(jsonPath("$[1].minimumVotesRequired").value(20))
+                .andExpect(jsonPath("$[1].votingOptionDto").isArray()); // votingOptionDto가 배열인지 확인
     }
+
+    /**
+     * topic id별로 반환
+     */
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    public void getOneTopic() throws Exception {
+        // given
+        Long topicId = 1L;
+        TopicResponse topicResponse = new TopicResponse(topicId, "파트장 투표", 10, new ArrayList<>());
+        given(topicService.getTopicById(topicId)).willReturn(topicResponse);
+
+        // when/then
+        mockMvc.perform(get("/api/topics/{id}", topicId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(topicId))
+                .andExpect(jsonPath("$.name").value("파트장 투표"))
+                .andExpect(jsonPath("$.minimumVotesRequired").value(10));
+    }
+
 }
