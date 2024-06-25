c2e01703984a8dd654b88dc54fe14da52c95e12d
sunghyun1356 <sunghyun1356@naver.com>
2024-06-25 18:26:04 +0900

    [fix] email fix

--- vote/src/main/java/com/ceos_19/vote/service/EmailServiceImpl.java
+++ vote/src/main/java/com/ceos_19/vote/service/EmailServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ceos_19.vote.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
@@ -13,37 +16,47 @@ import java.io.IOException;
 import java.util.Random;
 
 @Service
-public class EmailServiceImpl implements EmailService{
+public class EmailServiceImpl implements EmailService {
 
     @Autowired
-    JavaMailSender emailSender;
+    private JavaMailSender emailSender;
 
-    public static final String ePw = createKey();
+    private static final String ePw = createKey();
 
-    private MimeMessage createMessage(String to)throws Exception{
-        System.out.println("보내는 대상 : "+ to);
-        System.out.println("인증 번호 : "+ePw);
-        MimeMessage  message = emailSender.createMimeMessage();
+    private MimeMessage createMessage(String to) throws Exception {
+        // Extract email address from JSON string
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode;
 
-        message.addRecipients(MimeMessage.RecipientType.TO, to);//보내는 대상
-        message.setSubject("이메일 인증 테스트");//제목
+        try {
+            jsonNode = mapper.readTree(to);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Failed to parse JSON: " + e.getMessage(), e);
+        }
 
-        String msgg="";
-        msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> ceos vote 이메일 인증 코드입니다. </h1>";
-        msgg+= "<br>";
-        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
-        msgg+= "<br>";
-        msgg+= "<p>감사합니다.<p>";
-        msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= ePw+"</strong><div><br/> ";
-        msgg+= "</div>";
-        message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("rtssogang@google.com","ceos"));//보내는 사람
+        if (!jsonNode.has("email")) {
+            throw new IllegalArgumentException("The provided JSON does not contain an 'email' field.");
+        }
+
+        String emailAddress = jsonNode.get("email").asText();
+
+        System.out.println("Recipient: " + emailAddress);
+        System.out.println("Verification Code: " + ePw);
+
+        MimeMessage message = emailSender.createMimeMessage();
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailAddress)); // Correct way to add recipient
+        message.setSubject("Email Verification Test"); // Subject
+
+        String msgg = "<div style='margin:20px;'>" +
+                "<h1>CEOS Vote Email Verification Code</h1>" +
+                "<p>Please copy and enter the code below:</p>" +
+                "<p>Thank you.</p>" +
+                "<div align='center' style='border:1px solid black; font-family:verdana';>" +
+                "<h3 style='color:blue;'>Registration Verification Code</h3>" +
+                "<div style='font-size:130%'>CODE: <strong>" + ePw + "</strong></div></div>";
+
+        message.setText(msgg, "utf-8", "html"); // Content
+        message.setFrom(new InternetAddress("rtssogang@google.com", "CEOS")); // Sender
 
         return message;
     }
@@ -52,38 +65,40 @@ public class EmailServiceImpl implements EmailService{
         StringBuffer key = new StringBuffer();
         Random rnd = new Random();
 
-        for (int i = 0; i < 8; i++) { // 인증코드 8자리
-            int index = rnd.nextInt(3); // 0~2 까지 랜덤
+        for (int i = 0; i < 8; i++) { // Verification code with 8 characters
+            int index = rnd.nextInt(3); // 0~2 random
 
             switch (index) {
                 case 0:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
+                    key.append((char) ((int) (rnd.nextInt(26)) + 97)); // a~z
                     break;
                 case 1:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    //  A~Z
+                    key.append((char) ((int) (rnd.nextInt(26)) + 65)); // A~Z
                     break;
                 case 2:
-                    key.append((rnd.nextInt(10)));
-                    // 0~9
+                    key.append((rnd.nextInt(10))); // 0~9
                     break;
             }
         }
         return key.toString();
     }
+
     @Override
-    public String sendSimpleMessage(String to)throws Exception {
-        // TODO Auto-generated method stub
-        MimeMessage message = createMessage(to);
-        try{//예외처리
+    public String sendSimpleMessage(String to) throws Exception {
+        // Print input JSON for debugging
+        System.out.println("Input JSON: " + to);
+
+        MimeMessage message;
+        try {
+            message = createMessage(to);
             emailSender.send(message);
-        }catch(MailException es){
-            es.printStackTrace();
-            throw new IllegalArgumentException();
+        } catch (MailException | IllegalArgumentException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("Failed to send email: " + e.getMessage(), e);
         }
         return ePw;
     }
+
     private String mimeMessageToString(MimeMessage message) throws IOException, MessagingException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         message.writeTo(outputStream);
